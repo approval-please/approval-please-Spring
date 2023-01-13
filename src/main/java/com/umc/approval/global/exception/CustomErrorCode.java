4bcd219cce38eeeabc896fb3fe08ce017172959a
HunSeongPark <phunseong@gmail.com>
2023-01-13 15:39:47 +0900

    [FEAT #1] CustomErrorCode 추가

---
+++ src/main/java/com/umc/approval/global/exception/CustomErrorCode.java
@@ -0,0 +1,22 @@
+package com.umc.approval.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomErrorCode {
+
+    // TODO ErrorCode를 추가해주세요.
+    // EX)
+    // USER_NOT_FOUND(HttpStatus.NOT_FOUND, 10001, "사용자를 찾을 수 없습니다.")
+
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String errorMessage;
+
+    public int getStatusCode() {
+        return this.httpStatus.value();
+    }
+}
