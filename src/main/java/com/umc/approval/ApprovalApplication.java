7d9aa10baaa7abab34581ec9f2990b9a0797648f
HunSeongPark <phunseong@gmail.com>
2023-01-13 14:02:00 +0900

    initial commit

---
+++ src/main/java/com/umc/approval/ApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.umc.approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/umc/approval/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.umc.approval;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "health check ok v1";
+    }
+}
