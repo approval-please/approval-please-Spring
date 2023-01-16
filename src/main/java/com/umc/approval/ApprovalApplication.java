fa80901b5786f688493cdf27b243be81b4ac92bd
HunSeongPark <phunseong@gmail.com>
2023-01-16 14:10:53 +0900

    [CHORE #8] applicaion.yml AWS S3 관련 설정

--- src/main/java/com/umc/approval/ApprovalApplication.java
+++ src/main/java/com/umc/approval/ApprovalApplication.java
@@ -1,6 +1,5 @@
 package com.umc.approval;
 
-import com.umc.approval.global.type.SocialType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class ApprovalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApprovalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalApplication.class, args);
+    }
 
 }
