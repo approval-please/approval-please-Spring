2d5e61e0b959af31964d6566e9395052f406721e
HunSeongPark <phunseong@gmail.com>
2023-01-16 15:18:40 +0900

    [FEAT #10] SuccessHandler 구현

---
+++ src/main/java/com/umc/approval/global/security/handler/UserSuccessHandler.java
@@ -0,0 +1,50 @@
+package com.umc.approval.global.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.umc.approval.domain.user.entity.User;
+import com.umc.approval.domain.user.entity.UserRepository;
+import com.umc.approval.global.security.service.CustomUserDetails;
+import com.umc.approval.global.security.service.JwtService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+@Component
+public class UserSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        User user = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("가입된 이메일이 존재하지 않습니다."));
+
+        // JWT Token 생성 & Response
+        String accessToken = jwtService.createAccessToken(user.getEmail(), user.getId());
+        String refreshToken = jwtService.createRefreshToken(user.getEmail());
+        user.updateRefreshToken(refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getWriter(), new TokenResponseDto(accessToken, refreshToken));
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class TokenResponseDto {
+        private String accessToken;
+        private String refreshToken;
+    }
+}
