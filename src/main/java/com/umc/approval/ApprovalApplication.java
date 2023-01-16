f73fbbfe99fd78a41161912d8df167347542295e
HunSeongPark <phunseong@gmail.com>
2023-01-16 14:56:24 +0900

    [FEAT #10] AuthenticationFilter 구현

--- src/main/java/com/umc/approval/ApprovalApplication.java
+++ src/main/java/com/umc/approval/ApprovalApplication.java
@@ -2,7 +2,10 @@ package com.umc.approval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class ApprovalApplication {
         SpringApplication.run(ApprovalApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
