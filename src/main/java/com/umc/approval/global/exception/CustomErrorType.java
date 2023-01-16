1531c3fb902ba239eb6e584a7d6ed12c89d266f1
HunSeongPark <phunseong@gmail.com>
2023-01-16 14:33:06 +0900

    [FEAT #8] AwsS3Service 추가, 이미지 업로드/삭제 로직 구현

--- src/main/java/com/umc/approval/global/exception/CustomErrorType.java
+++ src/main/java/com/umc/approval/global/exception/CustomErrorType.java
@@ -12,6 +12,10 @@ public enum CustomErrorType {
 
     // TODO ErrorCode를 추가해주세요.
 
+    // Image (8xxxx)
+    IMAGE_UPLOAD_FAILED(BAD_REQUEST, 80001, "이미지 업로드에 실패했습니다."),
+    IMAGE_DELETE_FAILED(BAD_REQUEST, 80002, "이미지 삭제에 실패했습니다."),
+
     // General (9xxxx)
     INVALID_HTTP_METHOD(METHOD_NOT_ALLOWED, 90001, "잘못된 Http Method 요청입니다."),
     INVALID_VALUE(BAD_REQUEST, 90002, "잘못된 입력값입니다."),
