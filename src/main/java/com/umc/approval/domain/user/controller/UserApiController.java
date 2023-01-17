ad32505de39cc97fe116e20fec97e1aec3460250
HunSeongPark <phunseong@gmail.com>
2023-01-17 19:04:21 +0900

    [FEAT #15] refresh 엔트포인트 GET->POST 메소드 변경

--- src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
+++ src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
@@ -2,19 +2,12 @@ package com.umc.approval.domain.user.controller;
 
 import com.umc.approval.domain.user.dto.TokenResponseDto;
 import com.umc.approval.domain.user.service.UserService;
-import com.umc.approval.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-
-import static com.umc.approval.global.exception.CustomErrorType.TOKEN_NOT_EXIST;
-import static com.umc.approval.global.security.service.JwtService.TOKEN_HEADER_PREFIX;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,7 +21,7 @@ public class UserApiController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/refresh")
+    @PostMapping("/auth/refresh")
     public ResponseEntity<TokenResponseDto> refresh(HttpServletRequest request) {
         return ResponseEntity.ok(userService.refresh(request));
     }
