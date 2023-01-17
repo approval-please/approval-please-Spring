a4d3501bc41acb7ec8562170063596117b9dd2f9
HunSeongPark <phunseong@gmail.com>
2023-01-17 19:29:32 +0900

    [TEST #15] refresh 서비스 유닛테스트

--- src/test/java/com/umc/approval/unit/user/service/UserServiceTest.java
+++ src/test/java/com/umc/approval/unit/user/service/UserServiceTest.java
@@ -1,14 +1,12 @@
 package com.umc.approval.unit.user.service;
 
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.umc.approval.domain.user.dto.TokenResponseDto;
 import com.umc.approval.domain.user.entity.User;
 import com.umc.approval.domain.user.entity.UserRepository;
 import com.umc.approval.domain.user.service.UserService;
 import com.umc.approval.global.aws.service.AwsS3Service;
+import com.umc.approval.global.exception.CustomException;
 import com.umc.approval.global.security.service.JwtService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,12 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static com.umc.approval.global.exception.CustomErrorType.INVALID_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -56,22 +53,64 @@ public class UserServiceTest {
 
     @DisplayName("refresh token을 통해 access token 재발급에 성공한다")
     @Test
-    void refresh_success() {
+    void refresh_access_success() {
 
         // given
         User user = createUser(1L);
         String testToken = "test1234";
+        String responseAccessToken = "responseAccessToken";
         user.updateRefreshToken(testToken);
-        given(jwtService.verifyToken(ref))
+        given(jwtService.getEmail(any())).willReturn(user.getEmail());
         given(jwtService.getToken(any())).willReturn(testToken);
         given(jwtService.calculateRefreshExpiredDays(any())).willReturn(200L);
         given(userRepository.findByEmail(any())).willReturn(Optional.of(user));
+        given(jwtService.createAccessToken(any(), any())).willReturn(responseAccessToken);
 
         // when
         TokenResponseDto response = userService.refresh(new MockHttpServletRequest());
 
         // then
-        assertThat(response.getAccessToken()).isNotNull();
+        assertThat(response.getAccessToken()).isEqualTo(responseAccessToken);
         assertThat(response.getRefreshToken()).isNull();
+    }
+
+    @DisplayName("refresh시 refresh token 만료기간이 30일 미만이면 refresh token도 재발급한다")
+    @Test
+    void refresh_access_refresh_success() {
+
+        // given
+        User user = createUser(1L);
+        String testToken = "test1234";
+        String responseAccessToken = "responseAccessToken";
+        String responseRefreshToken = "responseRefreshToken";
+        user.updateRefreshToken(testToken);
+        given(jwtService.getEmail(any())).willReturn(user.getEmail());
+        given(jwtService.getToken(any())).willReturn(testToken);
+        given(jwtService.calculateRefreshExpiredDays(any())).willReturn(15L);
+        given(userRepository.findByEmail(any())).willReturn(Optional.of(user));
+        given(jwtService.createAccessToken(any(), any())).willReturn(responseAccessToken);
+        given(jwtService.createRefreshToken(any())).willReturn(responseRefreshToken);
+
+        // when
+        TokenResponseDto response = userService.refresh(new MockHttpServletRequest());
+
+        // then
+        assertThat(response.getAccessToken()).isEqualTo(responseAccessToken);
+        assertThat(response.getRefreshToken()).isEqualTo(responseRefreshToken);
+    }
+
+    @DisplayName("token 재발급 시 db 내 refresh token과 일치하지 않으면 실패한다")
+    @Test
+    void refresh_db_not_matched_fail() {
+        // given
+        User user = createUser(1L);
+        String testToken = "test1234";
+        user.updateRefreshToken(testToken);
+        given(jwtService.getEmail(any())).willReturn(user.getEmail());
+        given(jwtService.getToken(any())).willReturn(testToken + "dummy");
+
+        // when & then
+        CustomException e = assertThrows(CustomException.class, () -> userService.refresh(new MockHttpServletRequest()));
+        assertThat(e.getErrorType()).isEqualTo(INVALID_TOKEN);
     }
 }
