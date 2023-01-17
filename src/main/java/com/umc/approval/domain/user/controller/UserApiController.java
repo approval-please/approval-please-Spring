10048bea8a4d6e5f6142b434e70a5fed77e7872a
HunSeongPark <phunseong@gmail.com>
2023-01-17 18:48:42 +0900

    [FEAT #15] refresh 로직 엔드포인트 추가

--- src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
+++ src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
@@ -1,10 +1,19 @@
 package com.umc.approval.domain.user.controller;
 
 import com.umc.approval.domain.user.service.UserService;
+import com.umc.approval.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+import static com.umc.approval.global.exception.CustomErrorType.TOKEN_NOT_EXIST;
+import static com.umc.approval.global.security.service.JwtService.TOKEN_HEADER_PREFIX;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +25,10 @@ public class UserApiController {
     public ResponseEntity<Void> logout() {
         userService.logout();
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<Map<String, String>> refresh() {
+        return ResponseEntity.ok(userService.refresh());
     }
 }
