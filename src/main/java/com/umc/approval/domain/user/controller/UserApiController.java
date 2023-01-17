fd3dc89e2fb6126882d7c487e9ebf9a2d9c8470d
sharpie1330 <amberaplin14@gmail.com>
2023-01-17 23:27:16 +0900

    [FEAT #1]User 테이블에 단순 회원 등록 로직 구현

--- src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
+++ src/main/java/com/umc/approval/domain/user/controller/UserApiController.java
@@ -1,9 +1,11 @@
 package com.umc.approval.domain.user.controller;
 
+import com.umc.approval.domain.user.dto.UserCreateRequest;
 import com.umc.approval.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -11,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     private final UserService userService;
+
+    @PostMapping("/auth/signup")
+    public ResponseEntity<Void> signup(
+            @RequestBody final UserCreateRequest userCreateRequest
+    ) {
+        userService.signup(userCreateRequest);
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping("/auth/logout")
     public ResponseEntity<Void> logout() {
