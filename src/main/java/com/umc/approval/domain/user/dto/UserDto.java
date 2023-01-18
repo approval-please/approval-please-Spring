b5bb937b62f94e52991af4fdf735aa51815f5e67
HunSeongPark <phunseong@gmail.com>
2023-01-18 18:11:50 +0900

    [REFACTOR] 회원가입 서비스 로직 toEntity 메서드 변경

--- src/main/java/com/umc/approval/domain/user/dto/UserDto.java
+++ src/main/java/com/umc/approval/domain/user/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.umc.approval.domain.user.dto;
 
+import com.umc.approval.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,6 +13,18 @@ public class UserDto {
         private String email;
         private String password;
         private String phoneNumber;
+
+        public User toEntity(String encodedPassword) {
+            // 일반회원가입 최초 가입자는 level 0, promotionPoint 0L로 초기화
+            return User.builder()
+                    .nickname(this.getNickname())
+                    .email(this.getEmail())
+                    .password(encodedPassword)
+                    .phoneNumber(this.getPhoneNumber())
+                    .level(0)
+                    .promotionPoint(0L)
+                    .build();
+        }
     }
 
     @Getter
