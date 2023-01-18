2d52908f7bf00b3f4d773695d5a0eb82497971bc
eujjong18 <eujjong18@naver.com>
2023-01-19 02:52:59 +0900

    [FEAT #21] 결재하기 게시글 삭제 로직 구현

--- src/main/java/com/umc/approval/domain/image/entity/ImageRepository.java
+++ src/main/java/com/umc/approval/domain/image/entity/ImageRepository.java
@@ -1,6 +1,19 @@
 package com.umc.approval.domain.image.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query(value = "select image_url from image where document_id = :document_id", nativeQuery = true)
+    List<String> findImageUrl(@Param("document_id") Long documentId);
+
+    @Modifying
+    @Query(value = "delete from image where document_id = :document_id", nativeQuery = true)
+    void deleteByDocumentId(@Param("document_id") Long documentId);
 
 }
--- src/main/java/com/umc/approval/domain/tag/entity/TagRepository.java
+++ src/main/java/com/umc/approval/domain/tag/entity/TagRepository.java
@@ -1,7 +1,13 @@
 package com.umc.approval.domain.tag.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Modifying
+    @Query(value = "delete from tag where document_id = :document_id", nativeQuery = true)
+    void deleteByDocumentId(@Param("document_id") Long documentId);
 }
