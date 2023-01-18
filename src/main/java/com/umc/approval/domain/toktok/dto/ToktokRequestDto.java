4166179d2f696e12244c908938aa4ee5c6c39a21
JungYoonshin <sju08227@naver.com>
2023-01-18 09:55:14 +0900

    [FEAT #16] dto 카테고리 0-17까지 확인하는 유효성 검사 추가

--- src/main/java/com/umc/approval/domain/toktok/dto/ToktokRequestDto.java
+++ src/main/java/com/umc/approval/domain/toktok/dto/ToktokRequestDto.java
@@ -2,6 +2,7 @@ package com.umc.approval.domain.toktok.dto;
 
 import java.util.List;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,6 +16,8 @@ import org.w3c.dom.stylesheets.LinkStyle;
 public class ToktokRequestDto {
 
     @NotNull(message = "카테고리는 필수 입력값입니다.")
+    @Min(value = 0, message = "카테고리는 0부터 17까지의 정수 값입니다.")
+    @Max(value = 17, message = "카테고리는 0부터 17까지의 정수 값입니다.")
     private Integer category;
 
     @NotBlank(message = "내용은 필수 입력값입니다.")
