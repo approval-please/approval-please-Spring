b0e53cbc24e8986857d891ca570a3571e0a4f4ae
HunSeongPark <phunseong@gmail.com>
2023-01-18 13:14:39 +0900

    [FEAT] Document Controller 메서드 추출

--- src/main/java/com/umc/approval/domain/document/controller/DocumentController.java
+++ src/main/java/com/umc/approval/domain/document/controller/DocumentController.java
@@ -20,39 +20,17 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("documents")
+@RequestMapping("/documents")
 @RequiredArgsConstructor
 @Slf4j
 public class DocumentController {
+
     private final DocumentService documentService;
-    private final TagService tagService;
-    private final LinkService linkService;
-    private final ImageService imageService;
-    private final UserService userService;
 
-    @PostMapping("")
-    public ResponseEntity<?> createDocument(@Valid @RequestPart(value="data", required=false) DocumentRequest.PostDocumentRequest request,
+    @PostMapping
+    public ResponseEntity<Void> createDocument(@Valid @RequestPart(value="data", required=false) DocumentRequest.PostDocumentRequest request,
                                             @RequestPart(value="images", required=false) List<MultipartFile> images){
-
-        User user = userService.getUser();
-
-        // 해당하는 카테고리 찾기
-        CategoryType categoryType = Arrays.stream(CategoryType.values())
-                .filter(c -> c.getValue() == request.getCategory())
-                .findAny().get();
-
-        // 게시글 등록
-        Document newDocument = documentService.createDocument(request, categoryType, user);
-
-        if(request.getTag() != null)
-            tagService.createTag(request.getTag(), newDocument);
-        if(request.getLinkUrl() != null)
-            linkService.createLink(request.getLinkUrl(), newDocument);
-        if(images != null){
-            if(images.size()==1) { imageService.createImage(images.get(0), newDocument); }
-            else                 { imageService.createImage(images, newDocument);}
-        }
-
-        return ResponseEntity.ok().body("게시글 등록 성공!");
+        documentService.createDocument(request, images);
+        return ResponseEntity.ok().build();
     }
 }
