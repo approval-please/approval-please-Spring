52e0da2ed067e24a84e465a91024a595b0c84324
HunSeongPark <phunseong@gmail.com>
2023-01-18 16:18:59 +0900

    [STYLE] ToktokController 이름 변경

--- src/main/java/com/umc/approval/domain/toktok/controller/ToktokController.java
+++ src/main/java/com/umc/approval/domain/toktok/controller/ToktokController.java
@@ -1,13 +1,11 @@
 package com.umc.approval.domain.toktok.controller;
 
 import com.umc.approval.domain.toktok.dto.ToktokDto;
-import com.umc.approval.domain.toktok.entity.ToktokRepository;
 import com.umc.approval.domain.toktok.service.ToktokService;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -18,13 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-public class ToktokApiController {
+public class ToktokController {
 
-    @Autowired
     private final ToktokService toktokService;
-
-    @Autowired
-    private final ToktokRepository toktokRepository;
 
     @PostMapping("/community/toktoks")
     public ResponseEntity<Void> createPost(@Valid
--- src/main/java/com/umc/approval/domain/toktok/entity/ToktokRepository.java
+++ src/main/java/com/umc/approval/domain/toktok/entity/ToktokRepository.java
@@ -3,7 +3,6 @@ package com.umc.approval.domain.toktok.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ToktokRepository extends JpaRepository<Toktok, Long> {
 
 }
