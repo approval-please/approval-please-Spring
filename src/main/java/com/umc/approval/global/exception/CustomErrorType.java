3c50ef623a31f9c34763b0a8ee1340b53adaaf8a
eujjong18 <eujjong18@naver.com>
2023-01-19 10:52:00 +0900

    [FEAT #21] 사용자 인증, 삭제 권한 확인 코드 수정

--- src/main/java/com/umc/approval/global/exception/CustomErrorType.java
+++ src/main/java/com/umc/approval/global/exception/CustomErrorType.java
@@ -24,6 +24,7 @@ public enum CustomErrorType {
     USER_NOT_FOUND(NOT_FOUND, 20004, "사용자를 찾을 수 없습니다."),
     NO_PERMISSION(FORBIDDEN, 20005, "게시글 수정 및 삭제에 대한 권한이 없습니다."),
 
+    // Document (3xxxx)
     DOCUMENT_NOT_FOUND(NOT_FOUND, 30001, "존재하지 않는 결재서류입니다."),
 
 
