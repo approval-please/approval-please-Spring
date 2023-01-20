fe6eae0bee692335eadcf13d73a2c3f63847a1c1
HunSeongPark <phunseong@gmail.com>
2023-01-20 15:23:16 +0900

    [FEAT #39] 댓글 등록 유효성 검사

--- src/main/java/com/umc/approval/domain/comment/dto/CommentDto.java
+++ src/main/java/com/umc/approval/domain/comment/dto/CommentDto.java
@@ -3,6 +3,8 @@ package com.umc.approval.domain.comment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 public class CommentDto {
 
     @Getter
@@ -12,6 +14,7 @@ public class CommentDto {
         private Long reportId;
         private Long toktokId;
         private Long parentCommentId;
+        @NotBlank(message = "댓글의 내용은 필수 값입니다.")
         private String content;
     }
 }
