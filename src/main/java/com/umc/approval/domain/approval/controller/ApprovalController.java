039dd511f2779ca808668d3d7d50762b8c024d50
eujjong18 <eujjong18@naver.com>
2023-01-23 20:59:01 +0900

    [FEAT #52] 타 게시글 승인/반려 선택 로직 구현

---
+++ src/main/java/com/umc/approval/domain/approval/controller/ApprovalController.java
@@ -0,0 +1,25 @@
+package com.umc.approval.domain.approval.controller;
+
+import com.umc.approval.domain.approval.dto.ApprovalDTO;
+import com.umc.approval.domain.approval.service.ApprovalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ApprovalController {
+
+    private final ApprovalService approvalService;
+
+    @PostMapping("/documents/{documentId}")
+    public ResponseEntity<?> approveOtherDocument(@PathVariable("documentId") Long documentId,
+                                                  @RequestBody ApprovalDTO.PostApprovalRequest request){
+        ApprovalDTO.PostApprovalResponse response = approvalService.approveOtherDocument(request, documentId);
+        return ResponseEntity.ok().body(response);
+    }
+
+}
---
+++ src/main/java/com/umc/approval/domain/approval/dto/ApprovalDTO.java
@@ -0,0 +1,36 @@
+package com.umc.approval.domain.approval.dto;
+
+import com.umc.approval.domain.approval.entity.Approval;
+import com.umc.approval.domain.document.entity.Document;
+import com.umc.approval.domain.user.entity.User;
+import lombok.Data;
+
+public class ApprovalDTO {
+
+    // 타 게시글 승인/반려 request
+    @Data
+    public static class PostApprovalRequest{
+        Boolean isApprove;
+
+        public Approval toEntity(User user, Document document){
+            return Approval.builder()
+                    .user(user)
+                    .document(document)
+                    .isApprove(isApprove)
+                    .build();
+        }
+    }
+
+    // 타 게시글 승인/반려 response
+    @Data
+    public static class PostApprovalResponse{
+        Integer approveCount;
+        Integer rejectCount;
+
+        public PostApprovalResponse(int approveCount, int rejectCount){
+            this.approveCount = approveCount;
+            this.rejectCount = rejectCount;
+        }
+    }
+
+}
