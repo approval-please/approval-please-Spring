9d54fe8771991c0e7cd8a3048fd9a4a00779e161
HunSeongPark <phunseong@gmail.com>
2023-01-23 16:24:27 +0900

    [REFACTOR #55] 읽기전용 메서드 Transaction readOnly 설정

--- src/main/java/com/umc/approval/domain/like/service/LikeService.java
+++ src/main/java/com/umc/approval/domain/like/service/LikeService.java
@@ -5,6 +5,10 @@ import com.umc.approval.domain.follow.entity.FollowRepository;
 import com.umc.approval.domain.like.dto.LikeDto;
 import com.umc.approval.domain.like.entity.Like;
 import com.umc.approval.domain.like.entity.LikeRepository;
+import com.umc.approval.domain.user.entity.User;
+import com.umc.approval.domain.user.entity.UserRepository;
+import com.umc.approval.global.exception.CustomErrorType;
+import com.umc.approval.global.exception.CustomException;
 import com.umc.approval.global.security.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,6 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.umc.approval.global.exception.CustomErrorType.USER_NOT_FOUND;
+
 @Transactional
 @RequiredArgsConstructor
 @Service
@@ -24,7 +30,9 @@ public class LikeService {
     private final JwtService jwtService;
     private final LikeRepository likeRepository;
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public LikeDto.ListResponse getLikeList(HttpServletRequest request, Pageable pageable, LikeDto.Request requestDto) {
 
         Page<Like> likes = likeRepository.findAllByPost(pageable, requestDto);
@@ -50,5 +58,13 @@ public class LikeService {
                     .collect(Collectors.toList());
         }
         return LikeDto.ListResponse.from(likes, response);
+    }
+
+    public LikeDto.UpdateResponse like(LikeDto.Request requestDto) {
+
+        User user = userRepository.findById(jwtService.getId())
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+
     }
 }
