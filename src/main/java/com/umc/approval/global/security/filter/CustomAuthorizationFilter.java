140f12134d4a53933531125c06d7a19c33b8f52a
HunSeongPark <phunseong@gmail.com>
2023-01-23 16:21:43 +0900

    [FEAT #55] 좋아요 추가/취소 인증필터 추가

--- src/main/java/com/umc/approval/global/security/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/umc/approval/global/security/filter/CustomAuthorizationFilter.java
@@ -52,6 +52,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                         || (pathMatcher.match("/comments/**", path) && request.getMethod().equals("POST"))
                         || (pathMatcher.match("/comments/**", path) && request.getMethod().equals("PUT"))
                         || (pathMatcher.match("/comments/**", path) && request.getMethod().equals("DELETE"))
+                        || (pathMatcher.match("/likes/**", path) && request.getMethod().equals("POST"))
         );
     }
 
