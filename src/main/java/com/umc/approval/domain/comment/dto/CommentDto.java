725045483348fb3bf3429fbaaebbd9f13f14a44c
HunSeongPark <phunseong@gmail.com>
2023-01-23 18:39:59 +0900

    [FEAT #57] 댓글 목록조회 엔드포인트 추가

--- src/main/java/com/umc/approval/domain/comment/dto/CommentDto.java
+++ src/main/java/com/umc/approval/domain/comment/dto/CommentDto.java
@@ -2,14 +2,27 @@ package com.umc.approval.domain.comment.dto;
 
 import com.umc.approval.domain.comment.entity.Comment;
 import com.umc.approval.domain.document.entity.Document;
+import com.umc.approval.domain.like.dto.LikeDto;
+import com.umc.approval.domain.like.entity.Like;
 import com.umc.approval.domain.report.entity.Report;
 import com.umc.approval.domain.toktok.entity.Toktok;
 import com.umc.approval.domain.user.entity.User;
 import lombok.*;
+import org.springframework.data.domain.Page;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 public class CommentDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Request {
+        private Long documentId;
+        private Long toktokId;
+        private Long reportId;
+    }
 
     @Getter
     @Builder
@@ -37,6 +50,61 @@ public class CommentDto {
                     .imageUrl(imageUrl)
                     .build();
         }
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class ListResponse {
+        private Integer page;
+        private Integer totalPage;
+        private Long totalElement;
+        private List<ParentResponse> content;
+
+        public static LikeDto.ListResponse from(Page<Like> page, List<LikeDto.Response> content) {
+            return LikeDto.ListResponse.builder()
+                    .page(page.getNumber())
+                    .totalPage(page.getTotalPages())
+                    .totalElement(page.getTotalElements())
+                    .content(content)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class ParentResponse {
+        private Long commentId;
+        private Long userId;
+        private String profileImage;
+        private String nickname;
+        private Integer level;
+        private String content;
+        private List<ChildResponse> childComment;
+        private Boolean isWriter;
+        private Boolean isMy;
+        private Boolean isLike;
+        private Boolean isDeleted;
+        private Integer likeCount;
+        private String datetime;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class ChildResponse {
+        private Long commentId;
+        private Long userId;
+        private String profileImage;
+        private String nickname;
+        private Integer level;
+        private String content;
+        private Boolean isWriter;
+        private Boolean isMy;
+        private Boolean isLike;
+        private Integer likeCount;
+        private String datetime;
     }
 
     @Getter
