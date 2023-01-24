96886e9e4742954e1032ff2316d1959dd6d4e2a9
HunSeongPark <phunseong@gmail.com>
2023-01-24 22:06:30 +0900

    [FIX #76] 결재보고서 등록 s3 제거

--- src/main/java/com/umc/approval/domain/report/controller/ReportController.java
+++ src/main/java/com/umc/approval/domain/report/controller/ReportController.java
@@ -1,24 +1,15 @@
 package com.umc.approval.domain.report.controller;
 
-import com.umc.approval.domain.document.dto.DocumentDto;
-import com.umc.approval.domain.document.entity.Document;
 import com.umc.approval.domain.report.dto.ReportDto;
 import com.umc.approval.domain.report.service.ReportService;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/community/reports")
@@ -29,13 +20,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping
-    public ResponseEntity<Void> createPost(@Valid
-        @RequestPart(value = "data", required = false) ReportDto.ReportRequest request,
-        @RequestPart(value = "images", required = false) List<MultipartFile> files) {
-
-        reportService.createPost(request, files);
+    public ResponseEntity<Void> createPost(@Valid @RequestBody ReportDto.ReportRequest request) {
+        reportService.createPost(request);
         return ResponseEntity.ok().build();
-
     }
 
     // 결재서류 글 작성시 결재서류 선택 리스트
@@ -46,9 +33,9 @@ public class ReportController {
 
     @PutMapping("/{reportId}")
     public ResponseEntity<Void> updatePost(@Valid
-        @RequestPart(value = "data", required = false) ReportDto.ReportRequest request,
-        @RequestPart(value = "images", required = false) List<MultipartFile> files,
-        @PathVariable("reportId") Long id) {
+                                           @RequestPart(value = "data", required = false) ReportDto.ReportRequest request,
+                                           @RequestPart(value = "images", required = false) List<MultipartFile> files,
+                                           @PathVariable("reportId") Long id) {
 
         reportService.updatePost(id, request, files);
         return ResponseEntity.ok().build();
