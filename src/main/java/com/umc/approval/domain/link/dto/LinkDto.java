9056b796fc25c5284da2d18fdf510fe497abb8e8
eujjong18 <eujjong18@naver.com>
2023-01-25 16:32:12 +0900

    [FEAT #74] 링크 필수 값(url) 예외처리 추가

--- src/main/java/com/umc/approval/domain/link/dto/LinkDto.java
+++ src/main/java/com/umc/approval/domain/link/dto/LinkDto.java
@@ -3,13 +3,17 @@ package com.umc.approval.domain.link.dto;
 import com.umc.approval.domain.link.entity.Link;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 public class LinkDto {
 
     @Getter
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     @NoArgsConstructor
     public static class Request {
+        @NotBlank(message = "링크의 url은 필수 값입니다.")
         private String url;
+
         private String title;
         private String image;
     }
