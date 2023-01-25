45edc3d155d588c0513a4347798d27f17e52ed03
HunSeongPark <phunseong@gmail.com>
2023-01-25 21:51:45 +0900

    [FIX #96] 빌드 실패 오류 해결

--- src/main/java/com/umc/approval/domain/cert/controller/CertController.java
+++ src/main/java/com/umc/approval/domain/cert/controller/CertController.java
@@ -6,9 +6,10 @@ import com.umc.approval.domain.cert.service.CertService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
