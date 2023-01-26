7e709ca20da94a6f6fcccdaf46e2368fa65055a1
HunSeongPark <phunseong@gmail.com>
2023-01-26 18:54:27 +0900

    [TEST #112] 결재톡톡 검색 통합테스트

---
+++ src/test/java/com/umc/approval/integration/toktok/ToktokServiceIntegrationTest.java
@@ -0,0 +1,148 @@
+package com.umc.approval.integration.toktok;
+
+import com.umc.approval.domain.comment.entity.CommentRepository;
+import com.umc.approval.domain.image.entity.ImageRepository;
+import com.umc.approval.domain.like.entity.LikeRepository;
+import com.umc.approval.domain.link.entity.LinkRepository;
+import com.umc.approval.domain.scrap.entity.ScrapRepository;
+import com.umc.approval.domain.tag.entity.TagRepository;
+import com.umc.approval.domain.toktok.dto.ToktokDto;
+import com.umc.approval.domain.toktok.entity.Toktok;
+import com.umc.approval.domain.toktok.entity.ToktokRepository;
+import com.umc.approval.domain.toktok.service.ToktokService;
+import com.umc.approval.domain.user.entity.User;
+import com.umc.approval.domain.user.entity.UserRepository;
+import com.umc.approval.domain.vote.entity.UserVoteRepository;
+import com.umc.approval.domain.vote.entity.VoteOptionRepository;
+import com.umc.approval.domain.vote.entity.VoteRepository;
+import com.umc.approval.global.security.service.JwtService;
+import com.umc.approval.global.type.RoleType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Collections;
+import java.util.List;
+
+import static com.umc.approval.global.type.CategoryType.ANIMAL_PLANT;
+import static com.umc.approval.global.type.CategoryType.DIGITAL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class ToktokServiceIntegrationTest {
+
+    @Autowired
+    ToktokService toktokService;
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    ToktokRepository toktokRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    VoteOptionRepository voteOptionRepository;
+
+    @Autowired
+    LinkRepository linkRepository;
+
+    @Autowired
+    TagRepository tagRepository;
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    UserVoteRepository userVoteRepository;
+
+    @Autowired
+    LikeRepository likeRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    ScrapRepository scrapRepository;
+
+
+    private User createUser(Long id) {
+        return User.builder()
+                .email("test" + id + "@test.com")
+                .password("test123!")
+                .nickname("test" + id)
+                .phoneNumber("010-1234-5678")
+                .level(0)
+                .promotionPoint(0L)
+                .build();
+    }
+
+    private void loginUser(User user) {
+        String accessToken = jwtService.createAccessToken(user.getEmail(), user.getId());
+        List<SimpleGrantedAuthority> authorities
+                = Collections.singletonList(new SimpleGrantedAuthority(RoleType.USER.getKey()));
+        Authentication authToken = new UsernamePasswordAuthenticationToken(user.getEmail(), accessToken, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+    @DisplayName("결재톡톡 검색에 성공한다 - 카테고리 검색")
+    @Test
+    void search_toktok_success() {
+
+        // given
+        User user = createUser(1L);
+        userRepository.save(user);
+
+        Toktok toktok1 = Toktok.builder()
+                .user(user)
+                .category(DIGITAL)
+                .content("안녕하세요")
+                .view(0L)
+                .build();
+        toktokRepository.save(toktok1);
+
+        Toktok toktok2 = Toktok.builder()
+                .user(user)
+                .category(DIGITAL)
+                .content("안녕하소")
+                .view(0L)
+                .build();
+        toktokRepository.save(toktok2);
+
+        Toktok toktok3 = Toktok.builder()
+                .user(user)
+                .category(ANIMAL_PLANT)
+                .content("안녕하세요")
+                .view(0L)
+                .build();
+        toktokRepository.save(toktok3);
+
+        // when
+        ToktokDto.SearchResponse response = toktokService.search("하세", 0, DIGITAL.getValue(), 0);
+        ToktokDto.SearchListResponse findResponse = response.getContent().get(0);
+
+        // then
+        assertThat(response.getContent().size()).isEqualTo(1);
+        assertThat(findResponse.getCategory()).isEqualTo(DIGITAL.getValue());
+        assertThat(findResponse.getContent()).isEqualTo(toktok1.getContent());
+    }
+}
