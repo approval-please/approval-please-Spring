3b5f4a18db7fe3e56fcf328ffa9bf3f14a1e2673
HunSeongPark <phunseong@gmail.com>
2023-01-27 13:01:58 +0900

    [FEAT #117] 사원 검색 로직 구현

--- src/main/java/com/umc/approval/domain/profile/dto/ProfileDto.java
+++ src/main/java/com/umc/approval/domain/profile/dto/ProfileDto.java
@@ -1,11 +1,13 @@
 package com.umc.approval.domain.profile.dto;
 
+import com.umc.approval.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProfileDto {
 
@@ -15,6 +17,13 @@ public class ProfileDto {
     public static class SearchResponse {
         private Integer userCount;
         private List<SearchListResponse> content;
+
+        public static SearchResponse from(List<User> users) {
+            return SearchResponse.builder()
+                    .userCount(users.size())
+                    .content(users.stream().map(SearchListResponse::fromEntity).collect(Collectors.toList()))
+                    .build();
+        }
     }
 
     @Getter
@@ -25,5 +34,14 @@ public class ProfileDto {
         private String nickname;
         private String profileImage;
         private Integer level;
+
+        public static SearchListResponse fromEntity(User user) {
+            return SearchListResponse.builder()
+                    .userId(user.getId())
+                    .nickname(user.getNickname())
+                    .profileImage(user.getProfileImage())
+                    .level(user.getLevel())
+                    .build();
+        }
     }
 }
--- src/main/java/com/umc/approval/domain/user/entity/UserRepository.java
+++ src/main/java/com/umc/approval/domain/user/entity/UserRepository.java
@@ -1,7 +1,9 @@
 package com.umc.approval.domain.user.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPhoneNumber(String phoneNumber);
 
     Optional<User> findBySocialId(Long socialId);
+
+    List<User> findByNicknameContains(String query);
 }
