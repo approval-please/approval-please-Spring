d920682ac63ea88284d66dd9e7fcbef42921a936
Huh521 <huhgoo99@gmail.com>
2023-01-28 13:50:53 +0900

    [FEATURE #121] 마이페이지 - 사용자 프로필 조회 팔로잉 유무 처리

--- src/main/java/com/umc/approval/global/exception/CustomErrorType.java
+++ src/main/java/com/umc/approval/global/exception/CustomErrorType.java
@@ -22,7 +22,7 @@ public enum CustomErrorType {
     PHONE_NUMBER_ALREADY_EXIST(BAD_REQUEST, 20002, "이미 존재하는 전화번호입니다."),
     LOGIN_FAILED(UNAUTHORIZED, 20003, "아이디 또는 비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(NOT_FOUND, 20004, "사용자를 찾을 수 없습니다."),
-    NO_PERMISSION(FORBIDDEN, 20005, "게시글 수정 및 삭제에 대한 권한이 없습니다."),
+    NO_PERMISSION(FORBIDDEN, 20005, "수정 및 삭제에 대한 권한이 없습니다."),
     SNS_LOGIN_FAILED(UNAUTHORIZED, 20006, "SNS 로그인에 실패했습니다."),
     CERT_NOT_FOUND(NOT_FOUND, 20007, "전화번호 인증 요청 내역이 존재하지 않습니다."),
     CERT_FAILED(UNAUTHORIZED, 20008, "인증번호가 일치하지 않습니다."),
