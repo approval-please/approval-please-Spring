4b8c08a37980735178a01e33f779452a095472aa
HunSeongPark <phunseong@gmail.com>
2023-01-31 22:44:20 +0900

    [FEAT #165] 결재보고서 목록조회 로직 구현

--- src/main/java/com/umc/approval/domain/report/entity/ReportRepositoryImpl.java
+++ src/main/java/com/umc/approval/domain/report/entity/ReportRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.umc.approval.domain.follow.entity.Follow;
 import com.umc.approval.global.type.CategoryType;
 
 import javax.persistence.EntityManager;
@@ -11,10 +12,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.umc.approval.domain.document.entity.QDocument.document;
 import static com.umc.approval.domain.report.entity.QReport.report;
 import static com.umc.approval.domain.tag.entity.QTag.tag1;
+import static com.umc.approval.domain.toktok.entity.QToktok.toktok;
+import static com.umc.approval.domain.vote.entity.QVote.vote;
 
 public class ReportRepositoryImpl implements ReportRepositoryCustom {
 
@@ -79,6 +83,34 @@ public class ReportRepositoryImpl implements ReportRepositoryCustom {
         }
     }
 
+    @Override
+    public List<Report> findAllByOption(Long userId, List<Follow> follows, Integer sortBy) {
+        NumberExpression<Integer> date = new CaseBuilder()
+                .when(report.createdAt.after(LocalDate.now().minusDays(7).atTime(LocalTime.MIN)))
+                .then(1)
+                .otherwise(0);
+        return queryFactory
+                .selectFrom(report)
+                .innerJoin(report.document, document).fetchJoin()
+                .innerJoin(document.user).fetchJoin()
+                .leftJoin(report.likes)
+                .leftJoin(report.tags)
+                .leftJoin(document.tags)
+                .leftJoin(document.images)
+                .leftJoin(document.link).fetchJoin()
+                .leftJoin(report.links)
+                .leftJoin(report.images)
+                .leftJoin(report.comments)
+                .where(
+                        followingEq(follows, sortBy),
+                        userEq(userId, sortBy)
+                )
+                .distinct()
+                .orderBy(sortBy != null && sortBy == 0 ? date.desc() : report.createdAt.desc(),
+                        report.likes.size().add(report.comments.size()).add(report.view).desc())
+                .fetch();
+    }
+
     private BooleanExpression tagEq(String tag) {
         return tag1.tag.eq(tag);
     }
@@ -95,5 +127,15 @@ public class ReportRepositoryImpl implements ReportRepositoryCustom {
 
     private BooleanExpression contentLike(String query) {
         return report.content.contains(query);
+    }
+
+    private BooleanExpression followingEq(List<Follow> follows, Integer sortBy) {
+        return sortBy == null || sortBy != 1 ? null
+                : document.user.in(follows.stream().map(Follow::getToUser).collect(Collectors.toList()));
+    }
+
+    private BooleanExpression userEq(Long userId, Integer sortBy) {
+        return userId == null || sortBy == null || sortBy != 2 ? null
+                : document.user.id.eq(userId);
     }
 }
