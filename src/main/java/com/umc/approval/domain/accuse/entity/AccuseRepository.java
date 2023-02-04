7cae1be759c7c93cc4f0e12965cd56dcfafb1744
eujjong18 <eujjong18@naver.com>
2023-02-05 01:06:49 +0900

    [FIX #177] 결재서류 삭제 로직 수정

--- src/main/java/com/umc/approval/domain/accuse/entity/AccuseRepository.java
+++ src/main/java/com/umc/approval/domain/accuse/entity/AccuseRepository.java
@@ -2,6 +2,8 @@ package com.umc.approval.domain.accuse.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccuseRepository extends JpaRepository<Accuse, Long>, AccuseRepositoryCustom {
+import java.util.List;
 
+public interface AccuseRepository extends JpaRepository<Accuse, Long>, AccuseRepositoryCustom {
+    List<Accuse> findByDocumentId(Long documentId);
 }
