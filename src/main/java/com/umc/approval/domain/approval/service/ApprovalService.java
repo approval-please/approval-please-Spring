5207093a215cc95a9ab7a7df28bb863b9c0ac8be
HunSeongPark <phunseong@gmail.com>
2023-02-11 14:30:27 +0900

    [FEAT #184] 결재서류 최종결재 및 승인/반려 사용자 실적, 포인트 업데이트 로직 구현

--- src/main/java/com/umc/approval/domain/approval/service/ApprovalService.java
+++ src/main/java/com/umc/approval/domain/approval/service/ApprovalService.java
@@ -5,20 +5,22 @@ import com.umc.approval.domain.approval.entity.Approval;
 import com.umc.approval.domain.approval.entity.ApprovalRepository;
 import com.umc.approval.domain.document.entity.Document;
 import com.umc.approval.domain.document.entity.DocumentRepository;
+import com.umc.approval.domain.performance.entity.Performance;
+import com.umc.approval.domain.performance.entity.PerformanceRepository;
 import com.umc.approval.domain.user.entity.User;
 import com.umc.approval.domain.user.entity.UserRepository;
 import com.umc.approval.global.exception.CustomException;
 import com.umc.approval.global.security.service.JwtService;
+import com.umc.approval.global.type.PerformanceType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static com.umc.approval.global.exception.CustomErrorType.*;
+import static com.umc.approval.global.type.PerformanceType.*;
 
 @Transactional
 @RequiredArgsConstructor
@@ -28,6 +30,7 @@ public class ApprovalService {
     private final ApprovalRepository approvalRepository;
     private final UserRepository userRepository;
     private final DocumentRepository documentRepository;
+    private final PerformanceRepository performanceRepository;
 
     public ApprovalDTO.PostOtherApprovalResponse approveOtherDocument(ApprovalDTO.PostOtherApprovalRequest request,
                                                                       Long documentId) {
@@ -35,21 +38,21 @@ public class ApprovalService {
         User user = certifyUser();
         Document document = findDocument(documentId);
 
-        if(document.getUser().getId() == user.getId()){ // 내 게시글인 경우
+        if (document.getUser().getId() == user.getId()) { // 내 게시글인 경우
             throw new CustomException(CANNOT_APPROVE_MINE);
-        }else{ // 타 게시글인 경우
-            if(document.getState() == 2){ // 승인 대기 중인 경우
+        } else { // 타 게시글인 경우
+            if (document.getState() == 2) { // 승인 대기 중인 경우
                 int isApproved = approvalRepository.countByUserIdAndDocumentId(user.getId(), documentId);
-                if(isApproved == 0){
+                if (isApproved == 0) {
                     Approval approval = request.toEntity(user, document);
                     approvalRepository.save(approval);
 
                     // 포인트 적립
                     userRepository.updatePoint(user.getId(), 50L);
-                }else{
+                } else {
                     throw new CustomException(APPROVAL_ALREADY_EXISTS);
                 }
-            }else{
+            } else {
                 throw new CustomException(ALREADY_APPROVED);
             }
         }
@@ -60,40 +63,57 @@ public class ApprovalService {
         return new ApprovalDTO.PostOtherApprovalResponse(approveCount, rejectCount);
     }
 
-    public void approveMyDocument(ApprovalDTO.PostMyApprovalRequest request){
+    public void approveMyDocument(ApprovalDTO.PostMyApprovalRequest request) {
         User user = certifyUser();
         Document document = findDocument(request.getDocumentId());
 
-        if(document.getUser().getId() == user.getId()){ // 내 게시글인 경우
-            if(document.getState() == 2){ // 승인 대기 중인 경우
-                if(request.getIsApprove() == true){ // 최종 승인 처리
+        if (document.getUser().getId() == user.getId()) { // 내 게시글인 경우
+            if (document.getState() == 2) { // 승인 대기 중인 경우
+                if (request.getIsApprove()) { // 최종 승인 처리
                     documentRepository.updateState(document.getId(), 0);
-                }else{
+                } else {
                     documentRepository.updateState(document.getId(), 1);
                 }
 
-                // 작성자 포인트 적립
-                userRepository.updatePoint(user.getId(), 100L);
-                // 결재 참여자 포인트 적립
-                List<Long> userIdList = approvalRepository.findByDocumentIdAndIsApprove(document.getId(), request.getIsApprove());
-                userRepository.updatePoint(userIdList, 100L);
-            }else{
+                // 작성자 실적, 포인트 적립
+                Performance performance = Performance.builder()
+                        .user(user)
+                        .content(FINAL_APPROVE_MY_DOCUMENT.getContent())
+                        .point(FINAL_APPROVE_MY_DOCUMENT.getPoint())
+                        .build();
+                performanceRepository.save(performance);
+                user.updatePoint(FINAL_APPROVE_MY_DOCUMENT.getPoint());
+
+                // 결재 참여자 실적, 포인트 적립
+                List<User> approveUsers = userRepository.findByApprove(document.getId(), request.getIsApprove());
+                PerformanceType performanceType = request.getIsApprove() ?
+                        FINAL_APPROVE_OTHER_DOCUMENT : FINAL_REJECT_OTHER_DOCUMENT;
+                approveUsers.forEach(u -> {
+                    Performance p = Performance.builder()
+                            .user(u)
+                            .content(performanceType.getContent())
+                            .point(performanceType.getPoint())
+                            .build();
+                    performanceRepository.save(p);
+                    user.updatePoint(performanceType.getPoint());
+                });
+            } else {
                 throw new CustomException(ALREADY_APPROVED);
             }
-        }else{ // 타 게시글인 경우
+        } else { // 타 게시글인 경우
             throw new CustomException(CANNOT_APPROVE_OTHER);
         }
     }
 
-    private User certifyUser(){
+    private User certifyUser() {
         User user = userRepository.findById(jwtService.getId())
                 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
         return user;
     }
 
-    private Document findDocument(Long documentId){
+    private Document findDocument(Long documentId) {
         Optional<Document> document = documentRepository.findById(documentId);
-        if(document.isEmpty()){
+        if (document.isEmpty()) {
             throw new CustomException(DOCUMENT_NOT_FOUND);
         }
         return document.get();
