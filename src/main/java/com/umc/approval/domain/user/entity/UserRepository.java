ff6cca36891fd02b7b930604e4bda5b2b0b9310c
HunSeongPark <phunseong@gmail.com>
2023-02-11 14:15:05 +0900

    [FEAT #184] 결재서류 작성 시 실적, 포인트 업데이트 로직 구현

--- src/main/java/com/umc/approval/domain/user/entity/UserRepository.java
+++ src/main/java/com/umc/approval/domain/user/entity/UserRepository.java
@@ -17,12 +17,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialId(Long socialId);
 
     List<User> findByNicknameContains(String query);
-
-    @Modifying
-    @Query(value = "update users set promotion_point = promotion_point + :point where user_id = :user_id", nativeQuery = true)
-    void updatePoint(@Param("user_id") Long userId, @Param("point") Long point);
-
-    @Modifying
-    @Query(value = "update users set promotion_point = promotion_point + :point where user_id in (:user_id_list)", nativeQuery = true)
-    void updatePoint(@Param("user_id_list") List<Long> userIdList, @Param("point")Long point);
 }
--- src/main/java/com/umc/approval/global/type/PerformanceType.java
+++ src/main/java/com/umc/approval/global/type/PerformanceType.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PerformanceType {
 
-    WRITE_DOCUMENT("결재서류 작성"),
-    FINAL_APPROVE_MY_DOCUMENT("결재서류 최종 결재"),
-    APPROVE_OTHER_DOCUMENT("결재서류 승인/반려"),
-    FINAL_APPROVE_OTHER_DOCUMENT("승인한 서류가 최종 승인"),
-    FINAL_REJECT_OTHER_DOCUMENT("반려한 서류가 최종 반려"),
-    WRITE_REPORT("결재보고서 작성");
+    WRITE_DOCUMENT("결재서류 작성", 100),
+    FINAL_APPROVE_MY_DOCUMENT("결재서류 최종 결재", 100),
+    APPROVE_OTHER_DOCUMENT("결재서류 승인/반려", 50),
+    FINAL_APPROVE_OTHER_DOCUMENT("승인한 서류가 최종 승인", 100),
+    FINAL_REJECT_OTHER_DOCUMENT("반려한 서류가 최종 반려", 100),
+    WRITE_OTHER_REPORT("결재한 서류에 보고서가 작성됨", 200),
+    WRITE_REPORT("결재보고서 작성", 500);
 
     private final String content;
+    private final Integer point;
 }
