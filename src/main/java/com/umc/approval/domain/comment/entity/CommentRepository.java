906e7ab8e2b4285c504acd36480bd62c1472ee66
HunSeongPark <phunseong@gmail.com>
2023-05-01 21:43:46 +0900

    [DOCS] 리팩토링 기록 markdown 생성 및 추가

--- src/main/java/com/umc/approval/domain/comment/entity/CommentRepository.java
+++ src/main/java/com/umc/approval/domain/comment/entity/CommentRepository.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom, QuerydslPredicateExecutor<Comment> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
     @Query("select c from Comment c " +
             "join fetch c.user u " +
